d153ccafa54b053857d634793c199a7ced3527bf
Jack Crane <jack@jackcrane.rocks>
2023-11-02 16:53:48 -0500

    Update test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const child_process = require("child_process");
 
 const LOCAL_HAS_CHANGES =
-  child_process.execSync("git diff --name-only").toString() !== "" &&
+  child_process.execSync("git diff --name-only").toString() !== "" ||
   child_process
     .execSync("git ls-files . --exclude-standard --others")
     .toString() !== "";
