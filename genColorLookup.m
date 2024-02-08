05a82b5113cf6fac56d6541ffc876900c94116d9
Jack Crane <jack@jackcrane.rocks>
2024-02-08 17:33:37 -0600

    Tried normalizing

--- genColorLookup.m
+++ genColorLookup.m
@@ -9,21 +9,16 @@ cform = makecform('srgb2cmyk');
 CMYK_rep = applycform(RGB_Colormap,cform); 
 
 inprof = iccread('sRGB.icm');
-outprof = iccread('StratasysCMY.icc');
+outprof = iccread('profiles/StratasysCMY.icc');
 cform = makecform('icc',inprof,outprof);
 CMY_rep = applycform(RGB_Colormap,cform); 
 
 inprof = iccread('sRGB.icm');
-outprof = iccread('Stratasys_J8_7xx_VeroPureWhite_HT3_VividCMY.icm');
+outprof = iccread('profiles/Stratasys_J8_7xx_VeroPureWhite_HT3_VividCMY.icm');
 cform = makecform('icc',inprof,outprof);
 CMYW_rep = applycform(RGB_Colormap,cform); 
 
-inprof = iccread('sRGB.icm');
-outprof = iccread('Tavor_Xrite_i1Profiler_VividCMYW.icc');
-cform = makecform('icc',inprof,outprof);
-CMYW_xrite = applycform(RGB_Colormap,cform); 
-
-subplotCount = 7;
+subplotCount = 9;
 
 figure(1)
 subplot(subplotCount,1,1)
@@ -52,7 +47,7 @@ p2(3).Color = "y";
 p2(4).Color = "k";
 title('CMYK Representation')
 legend('Cyan','Mag','Yel','Key')
-csvwrite("lib/MatlabCMYK.csv", CMYK_rep);
+csvwrite("profiles/MatlabCMYK.csv", CMYK_rep);
 
 subplot(subplotCount,1,5)
 p3 = plot(CMY_rep);
@@ -62,7 +57,7 @@ p3(3).Color = "y";
 p3(4).Color = "k";
 title('Stratasys CMY Representation')
 legend('Cyan','Mag','Yel','Blank')
-csvwrite("lib/StratasysCMY.csv", CMY_rep);
+csvwrite("profiles/StratasysCMY.csv", CMY_rep);
 
 subplot(subplotCount,1,6)
 p4 = plot(CMYW_rep);
@@ -72,8 +67,13 @@ p4(3).Color = "y";
 p4(4).Color = "k";
 title('Stratasys PureWhite CMYW Representation')
 legend('Cyan','Mag','Yel','White')
-csvwrite("lib/StratasysPureWhiteCMYW.csv", CMYW_rep);
+csvwrite("profiles/StratasysPureWhiteCMYW.csv", CMYW_rep);
 
+
+inprof = iccread('sRGB.icm');
+outprof = iccread('profiles/Tavor_Xrite_i1Profiler_VividCMYW.icc');
+cform = makecform('icc',inprof,outprof);
+CMYW_xrite = applycform(RGB_Colormap,cform); 
 subplot(subplotCount,1,7)
 p4 = plot(CMYW_xrite);
 p4(1).Color = "c";
@@ -82,8 +82,52 @@ p4(3).Color = "y";
 p4(4).Color = "k";
 title('Tavor Xrite')
 legend('Cyan','Mag','Yel','White')
-csvwrite("lib/StratasysTavorXrite.csv", CMYW_xrite);
+csvwrite("profiles/StratasysTavorXrite.csv", CMYW_xrite);
 
+
+% inprof = iccread('sRGB.icm');
+% outprof = iccread('profiles/Colorimetric1mm_CMYW.icc');
+% cform = makecform('icc',inprof,outprof);
+% data = applycform(RGB_Colormap,cform); 
+% subplot(subplotCount,1,8)
+% p4 = plot(data);
+% p4(1).Color = "c";
+% p4(2).Color = "m";
+% p4(3).Color = "y";
+% p4(4).Color = "k";
+% title('Colorimetric1mm_CMYW')
+% legend('Cyan','Mag','Yel','White')
+% csvwrite("profiles/Colorimetric1mm_CMYW.csv", data);
+
+
+inprof = iccread('sRGB.icm');
+outprof = iccread('profiles/Colorimetric1mm.icc');
+cform = makecform('icc',inprof,outprof);
+data = applycform(RGB_Colormap,cform); 
+subplot(subplotCount,1,8)
+p4 = plot(data);
+p4(1).Color = "c";
+p4(2).Color = "m";
+p4(3).Color = "y";
+p4(4).Color = "k";
+title('Colorimetric1mm')
+legend('Cyan','Mag','Yel','White')
+csvwrite("profiles/Colorimetric1mm.csv", data);
+
+
+inprof = iccread('sRGB.icm');
+outprof = iccread('profiles/Perceptual1mm.icc');
+cform = makecform('icc',inprof,outprof);
+data = applycform(RGB_Colormap,cform); 
+subplot(subplotCount,1,9)
+p4 = plot(data);
+p4(1).Color = "c";
+p4(2).Color = "m";
+p4(3).Color = "y";
+p4(4).Color = "k";
+title('Perceptual1mm')
+legend('Cyan','Mag','Yel','White')
+csvwrite("profiles/Perceptual1mm.csv", data);
 
 
 figure(2)
